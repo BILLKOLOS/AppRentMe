93b1e029e195629dd2b459b5726a7c5c463f4b78
BILLKOLOS <117803237+BILLKOLOS@users.noreply.github.com>
2023-06-13 07:19:34 +0300

    Create Text_field_with_icon.kt
    Simple text field with icon

---
+++ Text_field_with_icon.kt
@@ -0,0 +1,51 @@
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.InternalTextApi
+import com.shubham.sc_prep.ui.theme.SCPrepTheme
+
+class MainActivity : AppCompatActivity() {
+    @InternalTextApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SCPrepTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextFieldWithIcon()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TextFieldWithIcon() {
+    var text by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    TextField(
+        value = text, onValueChange = { text = it },
+        placeholder = { Text(text = "Enter Name") },
+        leadingIcon = { Icon(Icons.Filled.Person, contentDescription = "Person Name") },
+    )
+}
