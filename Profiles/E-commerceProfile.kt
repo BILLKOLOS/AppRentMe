0dcc8c84376009ea9411651f8c279d81c7656d12
BILLKOLOS <117803237+BILLKOLOS@users.noreply.github.com>
2023-06-19 13:26:49 +0300

    Create E-commerceProfile.kt
    An e-commerce profile outlook

---
+++ Profiles/E-commerceProfile.kt
@@ -0,0 +1,166 @@
+import android.content.Context
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/*
+Helpful links to understand the code:
+
+TopAppBar:
+https://semicolonspace.com/jetpack-compose-topappbar/
+
+State in Jetpack Compose:
+https://semicolonspace.com/android-jetpack-compose-state/
+
+Gradient Backgrounds in Jetpack Compose:
+https://semicolonspace.com/android-jetpack-compose-gradient/
+
+Padding and Margin in Jetpack Compose:
+https://semicolonspace.com/jetpack-compose-padding-margin/
+
+ */
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlogPostsTheme(darkTheme = false) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // Top appbar
+                    TopAppbarProfile(context = LocalContext.current.applicationContext)
+
+                    ProfileEcommerce()
+                }
+            }
+        }
+    }
+}
+
+private val optionsList: ArrayList<OptionsData> = ArrayList()
+
+@Composable
+fun TopAppbarProfile(context: Context) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Profile",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        backgroundColor = MaterialTheme.colors.background,
+        elevation = 4.dp,
+        navigationIcon = {
+            IconButton(onClick = {
+                Toast.makeText(context, "Nav Button", Toast.LENGTH_SHORT).show()
+            }) {
+                Icon(
+                    Icons.Filled.ArrowBack,
+                    contentDescription = "Go back",
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun ProfileEcommerce(context: Context = LocalContext.current.applicationContext) {
+
+    // This indicates if the optionsList has data or not
+    // Initially, the list is empty. So, its value is false.
+    var listPrepared by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(Unit) {
+        withContext(Dispatchers.Default) {
+            optionsList.clear()
+
+            // Add the data to optionsList
+            prepareOptionsData()
+
+            listPrepared = true
+        }
+    }
+
+    if (listPrepared) {
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+            item {
+                // User's image, name, email and edit button
+                UserDetails(context = context)
+            }
+
+            // Show the options
+            items(optionsList) { item ->
+                OptionsItemStyle(item = item, context = context)
+            }
+
+        }
+    }
+}
+
+// This composable displays user's image, name, email and edit button
+@Composable
+private fun UserDetails(context: Context) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        // User's image
+        Image(
+            modifier = Modifier
+                .size(72.dp)
+                .clip(shape = CircleShape),
+            painter = painterResource(id = R.drawable.victoria),
+            contentDescription = "Your Image"
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(weight = 3f, fill = false)
+                    .padding(start = 16.dp)
+            ) {
